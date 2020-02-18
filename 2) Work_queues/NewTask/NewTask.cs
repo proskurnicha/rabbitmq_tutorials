3d7fc92a8fbab376f53e8da8ffa721ed7df7a49a
Natalie <natalia.proskurnicha@binary-studio.com>
2020-02-18 12:27:18 +0200

    Implementing work queues.

---
+++ 2) Work_queues/NewTask/NewTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace NewTask
+{
+    class Newtask
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "nata_queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = GetMessage(args);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properies = channel.CreateBasicProperties();
+                    properies.Persistent = true; 
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "nata_queue",
+                                         basicProperties: properies,
+                                         body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+        }
+    }
+}
