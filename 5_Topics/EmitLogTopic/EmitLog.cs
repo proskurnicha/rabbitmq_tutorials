f70db7bbe46acd00f355907411bfb17f122a6974
Natalie <proskurnicha@gmail.com>
2020-02-20 11:21:27 +0200

    Test commit.

--- 5_Topics/EmitLogTopic/EmitLog.cs
+++ 5_Topics/EmitLogTopic/EmitLog.cs
@@ -15,26 +15,23 @@ namespace EmitLog
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.ExchangeDeclare(exchange: "direct_logs",
-                                            type: ExchangeType.Direct);
+                    channel.ExchangeDeclare(exchange: "topic_logs",
+                                            type: ExchangeType.Topic);
 
-                    var severity = (args.Length > 0) ? args[0] : "info";
+                    var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
                     var message = (args.Length > 1)
-                                  ? string.Join(" ", args.Skip(1).ToArray())
-                                  : "Hello World!";
+                          ? string.Join(" ", args.Skip(1).ToArray())
+                          : "Hello World!";
 
                     var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish(exchange: "direct_logs",
-                                         routingKey: severity,
-                                         basicProperties: null,
-                                         body: body);
-
-                    Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+                    channel.BasicPublish(exchange: "topic_logs",
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
                 }
             }
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
         }
     }
 }
